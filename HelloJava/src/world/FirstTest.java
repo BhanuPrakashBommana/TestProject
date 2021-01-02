44a8a5a485df11ec45ba0424fcadc8e05c0284ef
bhbomman <bhbomman@LIN86000414.corp.capgemini.com>
2021-01-02 23:35:32 +0530

    First Test class is committed.

---
+++ HelloJava/src/world/FirstTest.java
@@ -0,0 +1,72 @@
+package world;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	public WebDriver driver;
+	
+  @BeforeClass
+  public void before() {
+	  
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");	
+
+		driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//Launching google site
+		
+		driver.get("https://github.com/login");
+  }
+  
+  @Test(priority=0)
+  
+  public void action(){
+	  
+	  String d=driver.getCurrentUrl();
+		
+		if(d.contains("https")){
+			
+			System.out.println("Site is secured");
+		}
+		
+		else{
+			System.out.println("Site is secured");
+		
+			}
+  }
+  
+  @Test(priority=1)
+  
+  public void login(){
+	  
+	  driver.findElement(By.xpath("//*[@name='login']")).sendKeys("bhanuprakash.bommana@gmail.com");
+	  
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  
+	  driver.findElement(By.xpath("//*[@name='passwor]")).sendKeys("bhanu@bbi_mca123");
+	  
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  
+	  driver.findElement(By.xpath("//*[@value='Sign in']")).click();
+	  
+  }
+  
+  @AfterClass
+  public void close(){
+	  
+	  driver.close();
+  }
+		
+		
+}
