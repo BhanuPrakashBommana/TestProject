b84c51eb07ea79c3063d421101fdb74aab199c63
bhbomman <bhbomman@LIN86000414.corp.capgemini.com>
2021-06-01 18:31:13 +0530

    Simple class file is commited

--- HelloJava/src/world/Simple.java
+++ HelloJava/src/world/Simple.java
@@ -1,5 +1,7 @@
 package world;
 
+import java.util.Scanner;
+
 import org.testng.reporters.jq.Main;
 
 public class Simple {
@@ -7,6 +9,34 @@ public class Simple {
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
+		
+		Scanner s=new Scanner(System.in);
+		
+		String x=s.nextLine();
+		
+		int n=Integer.parseInt(x);
+		
+		for(int i=1;i<=n;i++){
+			
+			if(i%3==0){
+				
+				System.out.print("Fizz"+" ");
+			}
+			
+			else if(i%5==0){
+				
+				System.out.print("Buzz"+" ");
+				
+			}
+			
+			else {
+				
+				System.out.print(i+" ");
+			}
+		
+		}
+		
+		
 	}
 	
 
